04219325a6b0def34caf53bcf1b21198718c9ed6
agustinromano <48917174+agustinromano@users.noreply.github.com>
2019-06-04 17:50:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 'use strict';
 
 function getFoodInfo() {
-  $('.js-form').on('submit', function(){
+  $('.js-form').on('submit', function (event) {
     event.preventDefault();
     const apiKey = 'RwF2lQB673HZPRaw9tmfa7arFAiPTb8e3Q1FSMh5';
     const url = `https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${apiKey} `;
+    $('#myTable').DataTable();
     fetch(url)
       .then(response => response.json())
-      .then(responseJson => displayResults(responseJson, food))
+      .then(responseJson => displayResults(responseJson))
   })
 }
 
-function displayResults() {
-
+function displayResults(test) {
+    $('#myTable').
+  console.log(test);
 }
 
-$(document).ready( function () {
-    $('#myTable').DataTable();
-} );
+$(document).ready(function () {
+  getFoodInfo();
+});
